19b716580787742eb6302e9dc1511c36ed8743b1
amanbolat <amanbolat@me.com>
2020-09-10 17:47:15 +0800

    init commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Xml.Linq;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+class Program {
+  private static void Main(string[] args) {
+    using var doc = WordprocessingDocument.Open("./testdoc.docx", false);
+    var reader = OpenXmlReader.Create(doc.MainDocumentPart.Document.Body);
+
+    while (reader.Read()) {
+      if (reader.IsEndElement) {
+        continue;
+      }
+      switch (reader.ElementType) {
+        case { } typ when typ == typeof(Paragraph) || typ == typeof(Body):
+          Console.WriteLine("Name: {0}. Depth: {1}", reader.LocalName, reader.Depth);
+          break;
+      }
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,21 @@
+How to run:
+```bash
+dotnet run
+```
+
+Result of run:
+```
+Name: body. Depth: 0
+Name: p. Depth: 1
+Name: p. Depth: 1
+Name: p. Depth: 1
+```
+
+
+What I would expect:
+```
+Name: body. Depth: 0
+Name: p. Depth: 0.0
+Name: p. Depth: 0.1
+Name: p. Depth: 0.2
+```
